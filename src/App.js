b7cc1145fd4a3b9795b6fd7c42a1f93164b5cb44
Juanjo <juanjofb86@gmail.com>
2019-08-31 09:57:25 +0200

    Instalar json-server y crear db en json

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+import axios from 'axios';
+
+import AgregarProducto from './componentes/AgregarProducto';
+import EditarProducto from './componentes/EditarProducto';
+import Productos from './componentes/Productos';
+import Producto from './componentes/Producto';
+
+import Header from './componentes/Header';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [ productos, guardarProductos ] = useState([]);
+
+	useEffect(() => {
+		const consultarApi = async () => {
+			// consultar la api del json-server
+			const resultado = await axios.get('http://localhost:4000/restaurant');
+
+			console.log(resultado.data);
+
+			guardarProductos(resultado.data);
+		}
+		consultarApi();
+	}, [])
+	return (
+		<Router>
+			<Header />
+			<main className="container mt-5">
+				<Switch>
+					<Route exac path="/nuevo-producto" component={AgregarProducto} />
+					<Route exac path="/productos" component={Productos} />
+					<Route exac path="/productos/:id" component={Producto} />
+					<Route exac path="/productos/editar/:id" component={EditarProducto} />
+				</Switch>
+			</main>
+			<p className="mt-4 p-2 text-center">Todos derechos reservados</p>
+		</Router>
+	);
 }
 
 export default App;
